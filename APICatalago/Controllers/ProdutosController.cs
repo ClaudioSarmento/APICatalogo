2abba449d5e8bbe291502153c4726b2bc5cdb1f8
claudio <claudio-s-i@hotmail.com>
2025-04-25 10:35:57 -0300

    feature - ProdutosController action Get(int id)

--- APICatalago/Controllers/ProdutosController.cs
+++ APICatalago/Controllers/ProdutosController.cs
@@ -19,9 +19,17 @@ namespace APICatalago.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            var produtos = _context.Produtos.ToList();
+            var produtos = _context?.Produtos?.ToList();
             if (produtos is null) return NotFound("Produtos não encontrados...");
             return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context?.Produtos?.FirstOrDefault(p => p.Id == id);
+            if (produto is null) return NotFound($"Produto {id} não encontrado...");
+            return produto;
         }
     }
 }
