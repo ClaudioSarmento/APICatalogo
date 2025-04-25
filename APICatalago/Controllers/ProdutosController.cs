108aedfc0652167419fba9f773e7ffb22418f5d3
claudio <claudio-s-i@hotmail.com>
2025-04-25 10:29:10 -0300

    feature - ProdutosController action Get()

---
+++ APICatalago/Controllers/ProdutosController.cs
@@ -0,0 +1,27 @@
+﻿using APICatalago.Domain.Entities;
+using APICatalago.Infrastructure.Data.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalago.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext? _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null) return NotFound("Produtos não encontrados...");
+            return produtos;
+        }
+    }
+}
