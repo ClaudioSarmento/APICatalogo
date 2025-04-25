168f156ff98c8b085dc15e290541198edb3ff28f
claudio <claudio-s-i@hotmail.com>
2025-04-25 13:31:36 -0300

    feature - ProdutosController action Delete()

--- APICatalago/Controllers/ProdutosController.cs
+++ APICatalago/Controllers/ProdutosController.cs
@@ -51,5 +51,15 @@ namespace APICatalago.Controllers
 
             return Ok(produto);
         }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context?.Produtos?.FirstOrDefault(p => p.Id==id);
+            if (produto is null || _context == null) return NotFound("Produto não localizado...");
+            _context.Produtos?.Remove(produto);
+            _context?.SaveChanges();
+            return Ok(produto);
+        }
     }
 }
