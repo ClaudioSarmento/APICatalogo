26f1dd2de4fd522f5511e99e3da581f15d73ea16
ClaudioSarmento <claudio-s-i@hotmail.com>
2025-04-26 15:06:03 -0300

    feature - CategoriasController action Get()

---
+++ APICatalago/Controllers/CategoriasController.cs
@@ -0,0 +1,29 @@
+﻿using APICatalago.Domain.Entities;
+using APICatalago.Infrastructure.Data.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalago.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext? _context;
+
+        public CategoriasController(AppDbContext? context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _context?.Categorias?.ToList();
+            if(categorias is null) return NotFound("Categorias não encontradas...");
+            return categorias;
+        }
+
+
+    }
+}
