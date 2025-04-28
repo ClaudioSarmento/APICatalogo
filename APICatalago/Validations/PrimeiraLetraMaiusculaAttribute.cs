e9088c7ff2e3b3291cfc738c8543bfa846a13482
ClaudioSarmento <claudio-s-i@hotmail.com>
2025-04-28 15:04:07 -0300

    feature - validação personalizada Produto.Nome

---
+++ APICatalago/Validations/PrimeiraLetraMaiusculaAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalago.Validations
+{
+    public class PrimeiraLetraMaiusculaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var texto = value?.ToString()?.TrimStart();
+
+            if (string.IsNullOrEmpty(texto) || char.IsUpper(texto[0]))
+                return ValidationResult.Success;
+
+            return new ValidationResult("A primeira letra do nome do produto deve ser maiúscula.");
+        }
+    }
+}
