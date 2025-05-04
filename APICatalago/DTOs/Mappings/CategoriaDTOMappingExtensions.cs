49d2ae34d2bf2c6d760e657f1a107ce6f6d39e47
ClaudioSarmento <claudio-s-i@hotmail.com>
2025-05-04 17:08:01 -0300

    feature - criando métodos de extensão para o mapeamento manual

---
+++ APICatalago/DTOs/Mappings/CategoriaDTOMappingExtensions.cs
@@ -0,0 +1,48 @@
+﻿using APICatalago.Domain.Entities;
+
+namespace APICatalago.DTOs.Mappings
+{
+    public static class CategoriaDTOMappingExtensions
+    {
+        public static CategoriaDTO? ToCategoriaDTO(this Categoria categoria)
+        {
+            if(categoria == null) return null;
+            var categoriaDto = new CategoriaDTO()
+            {
+                Id = categoria.Id,
+                Nome = categoria.Nome,
+                ImagemUrl = categoria.ImagemUrl
+
+            };
+            return categoriaDto;
+        }
+
+        public static Categoria? ToCategoria(this CategoriaDTO categoriaDto)
+        {
+            if (categoriaDto == null) return null;
+            var categoria = new Categoria()
+            {
+                Id = categoriaDto.Id,
+                Nome = categoriaDto.Nome,
+                ImagemUrl = categoriaDto.ImagemUrl
+            };
+            return categoria;
+        }
+
+        public static IEnumerable<CategoriaDTO> ToCategoriaDTOList(this IEnumerable<Categoria> categorias) 
+        {
+
+            var categoriasDto = categorias?
+                .Select(
+                    c => new CategoriaDTO
+                    {
+                        Id = c.Id,
+                        Nome = c.Nome,
+                        ImagemUrl = c.ImagemUrl,
+                    }
+                ) ?? [];
+            return categoriasDto;
+        }
+
+    }
+}
