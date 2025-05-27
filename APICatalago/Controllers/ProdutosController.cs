0c37c725be9d9e554f6aa35b2a99116eb038c208
ClaudioSarmento <claudio-s-i@hotmail.com>
2025-05-27 20:31:49 -0300

    Reapply "Versionamento da API usando Query String"
    This reverts commit c756e526b7845de5e15e5421674a13fcfe6f7547.

--- APICatalago/Controllers/ProdutosController.cs
+++ APICatalago/Controllers/ProdutosController.cs
@@ -14,6 +14,7 @@ namespace APICatalago.Controllers
     [Route("[controller]")]
     [ApiController]
     [Authorize]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class ProdutosController : ControllerBase
     {
         private IUnitOfWork _unitOfWork;
